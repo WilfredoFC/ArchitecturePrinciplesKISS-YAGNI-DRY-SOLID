6b5c6a1f403f3845f6c95bb20fd593a2f6b37dc8
WilfredoFC <180332250+WilfredoFC@users.noreply.github.com>
2025-01-31 18:03:51 -0400

    Subir todos los archivos.

---
+++ ArchitecturePrinciples/KISS.cs
@@ -0,0 +1,22 @@
+﻿namespace EjercicioKiss
+{
+    //Keep
+    //it
+    //simple
+    //stupid
+    public class RestaurantBill
+    {
+        public decimal CalculateTotal(decimal[] prices, decimal? tipPorcentaje = 10)
+        {
+            if(prices==null || prices.Length==0) return 0;
+
+            decimal totalPrices = prices.Sum();
+
+            decimal tip = totalPrices * (tipPorcentaje.GetValueOrDefault()/100);
+
+            return totalPrices + tip;
+
+        }
+
+    }
+}
